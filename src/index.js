7c2808d6515d146636b012884d94f04725380b54
rwik04 <129108609+rwik04@users.noreply.github.com>
2023-08-12 11:14:57 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./navbar.jsx";
+
+ReactDOM.render(
+    <Navbar></Navbar>, document.getElementById('root')
+);
---
+++ src/navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "./assets/images/logo.png" 
+import "./css/styles.css"
+
+function Navbar(){
+    return (
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src={logo} height="20px"/></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">MatchFeed</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">ScoreTrack</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">MatchdayBuzz</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search Football..." aria-label="Search"/>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+    )
+};
+
+export default Navbar;
